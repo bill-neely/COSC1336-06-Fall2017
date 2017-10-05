1a47a826a61a8c41c75dc2d2e4dcc24ff087f019
Bill Neely <bill.neely.tx@gmail.com>
2017-10-05 12:43:10 -0500

    Finish DealerUI

--- AutoDealer/src/DealerUI.java
+++ AutoDealer/src/DealerUI.java
@@ -1,16 +1,36 @@
+import java.util.Scanner;
 
 public class DealerUI {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		Dealership myDealer = Repository.getDealership();
 		boolean keepGoing = true;
 		while (keepGoing) {
 			displayCarLots(myDealer.CarLots());
-			keepGoing = false;
+			System.out.println("Please pick a Lot:");
+			String lotNumber = scan.nextLine();
+			if (!lotNumber.equals("exit")) {
+				if (validChoice(lotNumber, myDealer.CarLots().length)) {
+					int lotID = Integer.parseInt(lotNumber) - 1;
+					displayVehicles(myDealer.CarLots()[lotID].VehicleList());
+				}
+			}
+			else {
+				keepGoing = false;
+			}
 		}
 	}
-
 	
+	private static boolean validChoice(String lotNumber, int upperLimit) {
+		// TODO: Make sure lotNumber is an Integer
+		if (Integer.parseInt(lotNumber) > 0 && Integer.parseInt(lotNumber) < upperLimit + 1) {
+			return true;
+		}
+		return false;
+	}
+
+
 	private static void displayCarLots(CarLot[] theLots) {
 		for (int i = 0; i < theLots.length; i++) {
 			System.out.print((i + 1) + " - ");
