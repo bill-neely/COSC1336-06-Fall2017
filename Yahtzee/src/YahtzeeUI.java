eb6b4445c3640b61993371dc61ffd75f0137f5e0
Bill Neely <bill.neely.tx@gmail.com>
2017-11-28 12:31:37 -0600

    Implement 13 turns and 3 rolls

--- Yahtzee/src/YahtzeeUI.java
+++ Yahtzee/src/YahtzeeUI.java
@@ -4,26 +4,43 @@ public class YahtzeeUI {
 
 	public static void main(String[] args) {
 		Yahtzee theGame = new Yahtzee();
-		int counter = 0;
-		while (theGame.isNotOver() && counter<5) {
-			counter++;
-		//    takeATurn()
-		//    show the score card
-		//    ask - ready to take another turn?
+		while (theGame.isNotOver()) {
+			takeATurn(theGame);
+			//    show the score card
+			//    ask - ready to take another turn?
 		}
 		// displayFinalScore(theGame);
 		// if (!playAgain()) then quit
 	}
 
-	public static void takeATurn() {
-		// theGame.startTurnAndRollDice()
-		// showScoringOptions(theGame)
-		// showDice(theGame)
-		// if theGame.canRollAgain()
-		// 	choose a score or roll again
-		// else
-		//  choose a score
+	private static void displayDice(Yahtzee theGame) {
+		System.out.print("  ");
+		for (Die aDie : theGame.Dice()) {
+			System.out.print(aDie.sideUp + "\t");
+		}
+		System.out.println();
 	}
+
+	private static void pickAScore(Yahtzee theGame) {
+		theGame.pickScore();
+		System.out.println("   Pick A score");
+	}
+
+	public static void takeATurn(Yahtzee theGame) {
+		System.out.println("Start a Turn");
+		theGame.startATurn();
+		while (theGame.inATurn()) {
+			displayDice(theGame);
+			// showScoringOptions(theGame)
+			if (theGame.rollsLeft() > 0)
+				theGame.rollAgain();
+			else 
+				pickAScore(theGame);
+		}
+	}
+
+
+	
 	
 	public static void RollSomeDice() {
 		ArrayList<Die> myDice = new ArrayList<Die>(); 
@@ -38,12 +55,12 @@ public class YahtzeeUI {
 		}
 		return myDice;
 	}
-	
+
 	public static void printOut(ArrayList<Die> myList) {
-	    System.out.println("");
-	    System.out.println("List has size of: " + myList.size());  
-	    for (Die element : myList) {
-	      System.out.println("  " + element.sideUp);
-	    }
+		System.out.println("");
+		System.out.println("List has size of: " + myList.size());  
+		for (Die element : myList) {
+			System.out.println("  " + element.sideUp);
+		}
 	}
 }
