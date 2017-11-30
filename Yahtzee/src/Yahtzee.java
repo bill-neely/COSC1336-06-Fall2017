078d2ce1397c0c1d6bb89ca730edb08f14d4096b
Bill Neely <bill.neely.tx@gmail.com>
2017-11-30 15:16:47 -0600

    Updating files

--- Yahtzee/src/Yahtzee.java
+++ Yahtzee/src/Yahtzee.java
@@ -1,63 +1,13 @@
-import java.util.ArrayList;
-
 public class Yahtzee {
-	
-	private boolean inATurn;
-	private int turnsLeft;
-	private int rollsLeftInTurn;
-	private ArrayList<Die> dice;
+
+	private boolean isNotOver;
 
 	public Yahtzee() {
-		this.inATurn = false;
-		this.turnsLeft = 13;
-		this.rollsLeftInTurn = 0;
-		this.dice = new ArrayList<Die>();
-	}
-	
-	public void eliminateDie(String dieNumber) {
-		int number = Integer.parseInt(dieNumber);
-		this.dice.remove	(number - 1);
-	}
-
-	public boolean inATurn() {
-		return this.inATurn;
+		this.isNotOver = true;
 	}
 
 	public boolean isNotOver() {
-		return (this.turnsLeft > 0);
+		return this.isNotOver;
 	}
-
-	public void pickScore() {
-		this.inATurn = false;	
-		this.rollsLeftInTurn = 0;
-		this.dice.clear();
-	}
-
-	public void rollAgain() {
-		while (this.dice.size() < 5)
-			this.dice.add(new Die());
-		this.rollsLeftInTurn--;
-	}
-
-	public int rollsLeft() {
-		return this.rollsLeftInTurn;
-	}
-
-	public void startATurn() {
-		this.dice.clear();
-		this.turnsLeft--;
-		this.inATurn = true;
-		this.rollsLeftInTurn = 3;
-		this.rollAgain();
-	}
-	
-	public int turnsLeft() {
-		return this.turnsLeft;
-	}
-
-	public ArrayList<Die> Dice() {
-		return this.dice;
-	}
-
 
 }
