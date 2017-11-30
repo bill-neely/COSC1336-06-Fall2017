e9b96a338e167c3c9352b44faabda8ef1358685b
Bill Neely <bill.neely.tx@gmail.com>
2017-11-30 12:34:18 -0600

    Implement 13 turns, up to 3 rolls, preroll up to 5 dice each roll

--- Yahtzee/src/Yahtzee.java
+++ Yahtzee/src/Yahtzee.java
@@ -1,14 +1,63 @@
+import java.util.ArrayList;
 
 public class Yahtzee {
 	
-	private boolean isNotOver;
+	private boolean inATurn;
+	private int turnsLeft;
+	private int rollsLeftInTurn;
+	private ArrayList<Die> dice;
 
 	public Yahtzee() {
-		this.isNotOver = true;
+		this.inATurn = false;
+		this.turnsLeft = 13;
+		this.rollsLeftInTurn = 0;
+		this.dice = new ArrayList<Die>();
 	}
 	
-	public boolean isNotOver() {
-		return this.isNotOver;
+	public void eliminateDie(String dieNumber) {
+		int number = Integer.parseInt(dieNumber);
+		this.dice.remove	(number - 1);
 	}
+
+	public boolean inATurn() {
+		return this.inATurn;
+	}
+
+	public boolean isNotOver() {
+		return (this.turnsLeft > 0);
+	}
+
+	public void pickScore() {
+		this.inATurn = false;	
+		this.rollsLeftInTurn = 0;
+		this.dice.clear();
+	}
+
+	public void rollAgain() {
+		while (this.dice.size() < 5)
+			this.dice.add(new Die());
+		this.rollsLeftInTurn--;
+	}
+
+	public int rollsLeft() {
+		return this.rollsLeftInTurn;
+	}
+
+	public void startATurn() {
+		this.dice.clear();
+		this.turnsLeft--;
+		this.inATurn = true;
+		this.rollsLeftInTurn = 3;
+		this.rollAgain();
+	}
+	
+	public int turnsLeft() {
+		return this.turnsLeft;
+	}
+
+	public ArrayList<Die> Dice() {
+		return this.dice;
+	}
+
 
 }
--- Yahtzee/src/YahtzeeUI.java
+++ Yahtzee/src/YahtzeeUI.java
@@ -1,18 +1,36 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class YahtzeeUI {
-
+	
+	static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Yahtzee theGame = new Yahtzee();
-		while (theGame.isNotOver()) {
-			takeATurn(theGame);
-			//    show the score card
-			//    ask - ready to take another turn?
-		}
-		// displayFinalScore(theGame);
-		// if (!playAgain()) then quit
+		do {
+			Yahtzee theGame = new Yahtzee();
+			while (theGame.isNotOver()) {
+				System.out.println();
+				takeATurn(theGame);
+			}
+			displayFinalScore(theGame);
+		} while (playAgain());
+		scan.close();
+	}
+	
+	private static boolean playAgain() {
+		String choice = askForString("Play Again Y/N?");
+		return choice.toUpperCase().equals("Y");
 	}
 
+	private static void displayFinalScore(Yahtzee theGame) {
+		System.out.println("This is your final score!");
+	}
+
+	private static String askForString(String prompt) {
+		System.out.println(prompt);
+		return scan.nextLine();
+	}
+	
 	private static void displayDice(Yahtzee theGame) {
 		System.out.print("  ");
 		for (Die aDie : theGame.Dice()) {
@@ -27,21 +45,46 @@ public class YahtzeeUI {
 	}
 
 	public static void takeATurn(Yahtzee theGame) {
-		System.out.println("Start a Turn");
+		System.out.println("Start a Turn " + theGame.turnsLeft() + " turns left.");
 		theGame.startATurn();
 		while (theGame.inATurn()) {
 			displayDice(theGame);
 			// showScoringOptions(theGame)
-			if (theGame.rollsLeft() > 0)
-				theGame.rollAgain();
-			else 
+			if (theGame.rollsLeft() > 0) {
+				System.out.println(theGame.rollsLeft() + " rolls left.");
+				askEliminateOrPickScore(theGame);
+			} else 
 				pickAScore(theGame);
 		}
 	}
 
+	private static void askEliminateOrPickScore(Yahtzee theGame) {
+		String choice = askForString("    (P)ick a score, (R)oll Again");
+		if (choice.toUpperCase().equals("P"))
+			pickAScore(theGame);
+		else 
+			eliminateDice(theGame);
+	}
+	
+	private static void eliminateDice(Yahtzee theGame) {
+		// TODO: Get more than one at a time
+		boolean keepGoing = true;
+		while (keepGoing) {
+			String choice = askForString("      (1-" + theGame.Dice().size() + ") to throw away, (R)oll");
+			if (choice.toUpperCase().equals("R")) { 
+				theGame.rollAgain();
+				keepGoing = false;
+			} else {
+				theGame.eliminateDie(choice);
+				if (theGame.Dice().size() == 0) {
+					theGame.rollAgain();
+					keepGoing = false;
+				} else 
+					displayDice(theGame);
+			}
+		}
+	}
 
-	
-	
 	public static void RollSomeDice() {
 		ArrayList<Die> myDice = new ArrayList<Die>(); 
 		myDice = getDice(5);
